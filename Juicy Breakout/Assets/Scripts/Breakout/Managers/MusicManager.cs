213387ecef038340e370d7538bfec2f53e514215
Tapley123 <conortapley1@gmail.com>
2021-09-04 00:35:04 +0100

    random song changer

---
+++ Juicy Breakout/Assets/Scripts/Breakout/Managers/MusicManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public KeyCode changeSongButton;
+    public int currentSongIndex;
+    public List<AudioClip> songs;
+    [SerializeField] private GameObject currentSongDisplay;
+
+    [Header("Toggles")]
+    public bool displayCurrentSong = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+
+        PickStartSong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(changeSongButton))
+        {
+            PickRandomSong();
+        }
+
+        CurrentSongDisplay();
+    }
+
+    void PickStartSong() //picks a random song to play when the game starts
+    {
+        int randomSongIndex = Random.Range(0, songs.Count);
+        currentSongIndex = randomSongIndex;
+        audioSource.clip = songs[randomSongIndex];
+        audioSource.Play();
+    }
+
+    public void PickRandomSong()
+    {
+        bool foundNewIndex = false;
+        int tempNum;
+
+        while(!foundNewIndex)
+        {
+            tempNum = Random.Range(0, songs.Count);
+
+            if(tempNum != currentSongIndex)
+            {
+                foundNewIndex = true;
+                currentSongIndex = tempNum;
+                audioSource.clip = songs[tempNum];
+                audioSource.Play();
+            }
+        }
+    }
+
+    void CurrentSongDisplay()
+    {
+        if (displayCurrentSong)
+            currentSongDisplay.SetActive(true);
+        else
+            currentSongDisplay.SetActive(false);
+
+        currentSongDisplay.GetComponentInChildren<TMP_Text>().text = songs[currentSongIndex].name;
+    }
+}
--- Juicy Breakout/Assets/Scripts/Breakout/Paddle/Paddle.cs
+++ Juicy Breakout/Assets/Scripts/Breakout/Paddle/Paddle.cs
@@ -29,8 +29,8 @@ public class Paddle : MonoBehaviour
     private SpriteRenderer sr;
 
     private float yStartPos;
-    [SerializeField]private float halfPaddleSize;
-    [SerializeField]private float leftClamp, rightClamp;
+    private float halfPaddleSize;
+    private float leftClamp, rightClamp;
     #endregion
 
     void Start()
